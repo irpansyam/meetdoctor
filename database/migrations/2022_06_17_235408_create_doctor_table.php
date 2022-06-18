75db59df27253e69e6886658e01347be39cfc440
irpansyam <34511452+irpansyam@users.noreply.github.com>
2022-06-18 17:19:24 +0800

    Menambahkan tabel migration

--- database/migrations/2022_06_17_235408_create_doctor_table.php
+++ database/migrations/2022_06_17_235408_create_doctor_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('doctor', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->foreignId('current_team_id')->nullable();
-            $table->string('profile_photo_path', 2048)->nullable();
+            $table->string('fee');
+            $table->longText('photo')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -34,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('doctor');
     }
 };
